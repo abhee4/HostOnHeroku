b9b06c44d49ea0c152ac9bf8b7dc21f8a1dcbb17
inlabz <inlabz0102@gmail.com>
2020-02-22 17:10:15 +0530

    apiRoute

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('register', 'API\\Auth\\AuthController@register');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
