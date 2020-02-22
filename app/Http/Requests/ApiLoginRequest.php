ec4f6757566d9a4bb06ccecd47e91e7b24afd43c
inlabz <inlabz0102@gmail.com>
2020-02-23 02:03:24 +0530

    removed uniqueuser from APILoginController

--- app/Http/Requests/ApiLoginRequest.php
+++ app/Http/Requests/ApiLoginRequest.php
@@ -24,7 +24,7 @@ class ApiLoginRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users',],
+            'email' => ['required', 'string', 'email', 'max:255',],
             'password' => ['required', 'string', 'min:8',],
         ];
     }
