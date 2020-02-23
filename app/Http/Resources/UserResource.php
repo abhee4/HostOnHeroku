8e3e42744a266c0a24e2d348ad6d20ef5c6b3ea3
inlabz <inlabz0102@gmail.com>
2020-02-23 11:02:19 +0530

    resourceModels

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,13 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'user_id' => $this->id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'avatar'    => $this->avatar,
+            'mobile_number'    => $this->mobile,
+            'email'         => $this->email,
+        ];
     }
 }
